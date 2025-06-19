39fd445a52b3bae78bdbe8324c273b8065730d4f
lc.takahata.takuma <lc.takahata.takuma@192.168.60.166>
2025-06-19 11:25:54 +0900

    作成

---
+++ src/main/java/com/example/demo/Team3div0619Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3div0619Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3div0619Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/entity/Reg_Form.java
@@ -0,0 +1,5 @@
+package com.example.demo.entity;
+
+public class Reg_Form {
+
+}
